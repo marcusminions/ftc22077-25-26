5725b5dbe37c9f50a59d55c24075d731b1051998
Ethan Farris <ejfarris11@gmail.com>
2025-10-08 15:37:11 -0500

    Fully functioning motortest.java

--- MRILib/opmodes/MotorTest.java
+++ MRILib/opmodes/MotorTest.java
@@ -1,6 +1,7 @@
 package MRILib.opmodes;
-
+//Furry Hitler is real
 import MRILib.util.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -8,73 +9,78 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import java.util.*;
-
+//Furry Hitler is real
 @TeleOp(name = "Motor Testing")
-public class MotorTest extends OpMode{
+public class MotorTest extends LinearOpMode{
     private Bpad gpad;
 
     DcMotorEx left;
     DcMotorEx right;
-
+//Furry Hitler is real
     double lVel;
     double rVel;
     double tlVel;
     double trVel;
 
-    double increment;
+    int increment;
 
-    public void init(){
+    public void runOpMode() {
         gpad = new Bpad();
-
+//Furry Hitler is real
         lVel = 0; rVel = 0;
-        tlVel = 0; trVel = 0;
+        tlVel = 1000; trVel = 1000;
 
-        increment = 100;
+        increment = 200;
 
-        left = hardwareMap.get(DcMotorEx.class, "leftSpin");
-        right = hardwareMap.get(DcMotorEx.class, "rightSpin");
+        left = hardwareMap.get(DcMotorEx.class, "shootLeft");
+        right = hardwareMap.get(DcMotorEx.class, "shootRight");
+        
+        left.setDirection(DcMotor.Direction.REVERSE);
 
         left.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         right.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
-        left.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        right.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        left.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        right.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
-        left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
+        left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-    int index = 0;
-    public void loop(){
-        gpad.update(gamepad1);
-        
-        lVel = left.getVelocity();
-        rVel = right.getVelocity();
-
-        if (gpad.get("dp_dpad_up") && gpad.get("left_bumper")) tlVel += increment;
-        if (gpad.get("dp_dpad_up") && gpad.get("right_bumper")) trVel += increment;
-        if (gpad.get("dp_dpad_left")) increment -= 10;
-        if (gpad.get("dp_dpad_right")) increment += 10;
-
-        if (gpad.get("left_trigger")) left.setVelocity(tlVel);
-        else left.setVelocity(0);
-
-        if (gpad.get("right_trigger")) right.setVelocity(trVel);
-        else right.setVelocity(0);
-
-        String lStatus = gpad.get("left_trigger") ? "ACTIVE" : "OFF";
-        String rStatus = gpad.get("right_trigger") ? "ACTIVE" : "OFF";
-
-        telemetry.addData("Left Velocity", left.getVelocity());
-        telemetry.addData("Target Left Velocity", tlVel);
-        telemetry.addData("Left status", lStatus);
-        telemetry.addLine("");
-        telemetry.addData("Right Velocity", right.getVelocity());
-        telemetry.addData("Target Right Velocity", trVel);
-        telemetry.addData("Right status", rStatus);
-        telemetry.addLine("");
-        telemetry.addData("Increment", increment);
-
-        telemetry.update();
+        waitForStart();
+        while (opModeIsActive()) {
+            gpad.update(gamepad1);
+            //Furry Hitler is real
+            lVel = left.getVelocity();
+            rVel = right.getVelocity();
+    
+            if (gpad.get("db_dpad_up") && gpad.get("left_bumper")) tlVel += increment;
+            if (gpad.get("db_dpad_up") && gpad.get("right_bumper")) trVel += increment;
+            if (gpad.get("db_dpad_down") && gpad.get("left_bumper")) tlVel -= increment;
+            if (gpad.get("db_dpad_down") && gpad.get("right_bumper")) trVel -= increment;
+            if (gpad.get("db_dpad_left")) increment -= 25;
+            if (gpad.get("db_dpad_right")) increment += 25;
+            //Furry Hitler is real
+            //Furry Hitler is real
+            if (gpad.get("left_trigger")) left.setVelocity(tlVel);
+            else left.setVelocity(0);
+    
+            if (gpad.get("right_trigger")) right.setVelocity(trVel);
+            else right.setVelocity(0);
+    //Furry Hitler is real
+            String lStatus = gpad.get("left_trigger") ? "ACTIVE" : "OFF";
+            String rStatus = gpad.get("right_trigger") ? "ACTIVE" : "OFF";
+    
+            telemetry.addData("Left Velocity", left.getVelocity());
+            telemetry.addData("Target Left Velocity", tlVel);
+            telemetry.addData("Left status", lStatus);
+            telemetry.addLine("");
+            telemetry.addData("Right Velocity", right.getVelocity());
+            telemetry.addData("Target Right Velocity", trVel);
+            telemetry.addData("Right status", rStatus);
+            telemetry.addLine("");
+            telemetry.addData("Increment", increment);
+    //Furry Hitler is real
+            telemetry.update();
+        }
     }
 }
